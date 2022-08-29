886642ef96fab2a99c84a9455cb05312b1cc9259
dvdr1029 <67302404+dvdr1029@users.noreply.github.com>
2022-08-29 00:25:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -139,3 +139,37 @@ document.querySelectorAll(".operationbuttons")[7].addEventListener("click", func
 document.querySelector('.close-stats').addEventListener('click',function() {
   document.querySelector('.bg-modal-stats').style.display = "none";
 })
+
+
+(function() {
+    
+  var win = window,
+      doc = win.document;
+
+  // If there's a hash, or addEventListener is undefined, stop here
+  if ( !location.hash || !win.addEventListener ) {
+
+    //scroll to 1
+    window.scrollTo( 0, 1 );
+    var scrollTop = 1,
+
+    //reset to 0 on bodyready, if needed
+    bodycheck = setInterval(function(){
+      if( doc.body ){
+        clearInterval( bodycheck );
+        scrollTop = "scrollTop" in doc.body ? doc.body.scrollTop : 1;
+        win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+      } 
+    }, 15 );
+
+    if (win.addEventListener) {
+      win.addEventListener("load", function(){
+        setTimeout(function(){
+          //reset to hide addr bar at onload
+          win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+        }, 0);
+      }, false );
+    }
+  }
+
+})();
